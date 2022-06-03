4ce077209c5966530e9ab0da43c9bad5cfe89416
Robel-rai <robelabera02@gmail.com>
2022-06-03 13:28:23 -0700

    Size of command

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,6 +5,10 @@
  */
 int main(void)
 {
-	printf("Size of a char: 1 byte(s)\nSize of an int: 4 byte(s)\nSize of a long int: 4 byte(s)\nSize of a long long int: 8 byte(s)\nSize of a float: 4 byte(s)");
+	printf("Size of a char:" sizeof(char) " byte(s)\n"
+	printf("Size of an int:" sizeof(int) " byte(s)\n"
+	printf("Size of a long int:" sizeof(long) " byte(s)\n"
+	printf("Size of a long long int:" sizeof(long long) " byte(s)\n"
+	printf("Size of a float:" sizeof(float) " byte(s)");
 	return (0);
 }
