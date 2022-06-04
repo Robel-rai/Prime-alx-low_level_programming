b9eb16ff23785619e54c7b0566c1b1231f2d011b
Robel-rai <robelabera02@gmail.com>
2022-06-04 13:15:50 -0700

    Monor Tweak 2

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,10 +19,10 @@ int main(void)
 	printf("Last digit of %i is %i", n, n%10);
 	
 		if (Ld > 5)
-			printf(" and is greater than 5");
+			printf(" and is greater than 5\n");
 		else if (Ld == 0)
-			printf(" and is 0");
+			printf(" and is 0\n");
 		else
-			printf(" and is less than 6 and not 0");
+			printf(" and is less than 6 and not 0\n");
 	return (0);
 }
