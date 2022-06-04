97f8762424ad3800fff5543d90f6fecfa1edbbdb
Robel-rai <robelabera02@gmail.com>
2022-06-04 12:02:37 -0700

    Pos, Nev or Zer

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,15 +1,12 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-
 /**
  * main - main block
  * Description: takes a random number n 
  * then displays if it is Posetive, Negative or Zero
  * Returns : 0
 */
-
 int main(void)
 {
 	int n;
@@ -23,6 +20,5 @@ int main(void)
 			printf("%i is zero\n", n);
 		else
 			printf("%i is negative\n", n);
-		
 	return (0);
 }
