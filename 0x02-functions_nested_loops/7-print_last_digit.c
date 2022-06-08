39419eb8720801e23c8924c659bc42bb02ab2c45
Robel-rai <robelabera02@gmail.com>
2022-06-08 12:43:17 -0700

    There are only 3 C, 10 d and 7 n

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,11 +3,19 @@
 /**
  * print_last_digit - prints the last digit
  * Description: Tells the last digit of our function
+ * @b - is an integr
  * Return: 0
  */
 
 int print_last_digit(int b)
 {
-	int lasdi = b%10;
+	int lasdi;
+	
+	if (b < 0)
+	{
+		b = b * -1;
+	}
+	lasdi = b%10;
+	_putchar(lasdi + '0')
 	return (lasdi);
 }
