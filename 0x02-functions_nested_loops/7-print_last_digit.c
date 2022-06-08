cfd5fa33de6c3db9f94d53979c6efecf6897e33d
Robel-rai <robelabera02@gmail.com>
2022-06-08 12:52:34 -0700

    hello betty

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,14 +3,14 @@
 /**
  * print_last_digit - prints the last digit
  * Description: Tells the last digit of our function
- * @b - is an integr
+ * @b - b is an integr
  * Return: 0
  */
 
 int print_last_digit(int b)
 {
 	int lasdi;
-	
+
 	lasdi = b % 10;
 	if (lasdi < 0)
 	{
