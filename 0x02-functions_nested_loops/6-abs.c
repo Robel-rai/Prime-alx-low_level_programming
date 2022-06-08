87db19ec8cfa4a4c34deb597e77d3510ee2d1cb1
Robel-rai <robelabera02@gmail.com>
2022-06-08 12:23:38 -0700

    Absolute value

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -12,14 +12,14 @@ int _abs(int a)
 {
 	if (a < 0)
 	{
-		_putchar(a - (a * a));
+		return (a + (a * a));
 	}
 	else if (a==0)
 	{
-		_putchar('0');
+		return ('0');
 	}
 	else
 	{
-		_putchar(a + (a * a));
+		return (a);
 	}
 }
