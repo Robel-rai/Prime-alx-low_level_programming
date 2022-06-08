93ef7a160fc62d8f056650003fb7dcdb1d5eecb1
Robel-rai <robelabera02@gmail.com>
2022-06-08 02:23:23 -0700

    Here we go again

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,10 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - print alphabet
+ * print_alphabet - print alphabet
  * Description: prints the alpha bet
  * Return: 0
  */
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
 void print_alphabet(void)
 {
 	char le;
