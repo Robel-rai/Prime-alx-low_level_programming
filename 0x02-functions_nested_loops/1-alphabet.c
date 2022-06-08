64711d32a5538a8004acaa0226e6915b40ed3545
Robel-rai <robelabera02@gmail.com>
2022-06-08 02:00:21 -0700

    Alphabet using a function

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/*
+ * main - prints the code
+ * Description: writes the alplabet in lower case from a to z
+ * Return: 0
+ */
+int print_alphabet;
+int main()
+{
+	print_alphabet();
+	putchar('\n');
+	return (0);
+}
+
+int print_alphabet(){
+	char x;
+	for (x = 'a';x <= 'z';++x)
+	{
+		putchar(x);
+	}
+}
