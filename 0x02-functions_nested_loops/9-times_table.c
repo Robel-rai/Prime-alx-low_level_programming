8d2c8db58b479e6aeea13e26fadbe31a6e2b1d17
Robel-rai <robelabera02@gmail.com>
2022-06-12 23:21:05 -0700

    Time table 9*9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * time_table: prints out the time able of 9*9
+ *
+ * return:
+ */
+
+void time_table(void)
+{
+	int x;
+	int y;
+	int mul;
+
+	for (x = 0; x < 10; x++){
+	       for (y = 0; y <10; y++)
+	       {
+		       mul = x * y;
+		       if (mul < 10)
+		       {
+			       _putchar(mul + '0');
+			       _putchar(',');
+			       _putchar(' ');
+			       _putchar(' ');
+		       }
+		       ele if (mul >= 10)
+		       {
+			       _putchar(mul + '0');
+			       _putchar(',');
+			       _putchar(' ');
+		       }
+	       }
+	       _putchar('\n');
+	}
+}
