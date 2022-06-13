77c5586f04ca764d0cff1fa7b586ed0d6fdbccca
Robel-rai <robelabera02@gmail.com>
2022-06-12 23:32:33 -0700

    Error edited

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -31,7 +31,7 @@ void times_table(void)
 				_putchar((mul / 10) + '0');
 				_putchar((mul % 10) + '0');
 			}
-			else if ((product < 10) && (y != 0))
+			else if ((mul < 10) && (y != 0))
 			{
 				_putchar(' ');
 				_putchar((mul % 10) + '0');
