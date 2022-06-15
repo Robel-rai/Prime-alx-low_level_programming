3f7ccd671318105ed74d5d1d4cfb3eced7091e24
Robel-rai <robelabera02@gmail.com>
2022-06-15 04:59:34 -0700

    Sup

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -9,11 +9,13 @@
 int main(void)
 {
 	unsigned long int sum3 ,sum5, sum;
+	int i;
+
 	sum3 = 0;
 	sum5 = 0;
 	sum = 0;
 
-	for (int i = 0; i < 1024; ++i)
+	for (i = 0; i < 1024; ++i)
 	{
 		if ((i % 3) == 0)
 		{
